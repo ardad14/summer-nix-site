9e824c644d20f50484d1f5a0fab4a24d09d1095a
Artem Kuptsov <kuptsov210@gmail.com>
2021-07-29 19:23:06 +0300

    Create BookService for work with front

---
+++ App/Service/BookService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Model\BookModel;
+use App\Mapper\BookMapper;
+
+class BookService
+{
+    private BookModel $bookModel;
+    private BookMapper $bookMapper;
+
+    public function __construct()
+    {
+        $this->bookModel = new BookModel();
+        $this->bookMapper = new BookMapper();
+    }
+
+    public function getAll(): array
+    {
+        return $this->bookMapper->mapBook($this->bookModel->getAll());
+    }
+
+    public function getBySlug($slug): array
+    {
+        return $this->bookMapper->mapBook($this->bookModel->getBySlug($slug));
+    }
+}
