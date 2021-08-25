67f0d09d80251f7ed8be1a06a0a4960886c277f3
Artem Kuptsov <kuptsov210@gmail.com>
2021-08-25 20:14:39 +0300

    Edit authentication to work with admin

--- Framework/Authentication/Authentication.php
+++ Framework/Authentication/Authentication.php
@@ -2,6 +2,7 @@
 
 namespace Framework\Authentication;
 
+use Framework\Helpers\Exceptions\UserExceptions\NotSuchUserException;
 use Framework\Session\Session;
 use App\Service\UserService;
 use Framework\Core\Validator\Validator;
@@ -10,11 +11,13 @@ class Authentication
 {
     public Session $session;
     private Validator $validator;
+    private UserService $userService;
 
     public function __construct()
     {
         $this->session = Session::getInstance();
         $this->validator = new Validator();
+        $this->userService = new UserService();
     }
 
     public function isAuth(): bool
@@ -22,18 +25,43 @@ class Authentication
         return $this->session->containsKey("userName");
     }
 
+    public function isAdminAuth(): bool
+    {
+        return $this->session->containsKey("adminName");
+    }
+
     public function auth($login, $pass): bool
     {
-        $userService = new UserService();
-        $user = $userService->getByLogin($login);
-        if ($user != null && $user[0]->getlogin() === $login && $pass === $user[0]->getPassword()) {
-            $this->session->setKey("userName", $user[0]->getLogin());
-            $this->session->deleteKey("wrongCredentials");
-            return true;
+        try {
+            $user = $this->userService->getByField(["login" => $login]);
+            if ($user != null && $user[0]->getlogin() === $login && $pass === $user[0]->getPassword()) {
+                $this->session->setKey("userName", $user[0]->getLogin());
+                $this->session->deleteKey("wrongCredentials");
+                return true;
+            }
+            $this->validator->setLoginError();
+            return false;
+        } catch (NotSuchUserException $e) {
+            $this->validator->setLoginError();
+            return false;
         }
+    }
 
-        $this->validator->setLoginError();
-        return false;
+    public function authAdmin($login, $pass): bool
+    {
+        try {
+            $user = $this->userService->getByField(["login" => $login], "admin");
+            if ($user != null && $user[0]->getlogin() === $login && $pass === $user[0]->getPassword()) {
+                $this->session->setKey("adminName", $user[0]->getLogin());
+                $this->session->deleteKey("adminWrongCredentials");
+                return true;
+            }
+            $this->validator->setAdminLoginError();
+            return false;
+        } catch (NotSuchUserException $e) {
+            $this->validator->setAdminLoginError();
+            return false;
+        }
     }
 
     public function getLogin(): ?string
