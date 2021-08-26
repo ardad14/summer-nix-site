fe85f55e79e194cccbb382cd4f195c8b1a64525a
Artem Kuptsov <kuptsov210@gmail.com>
2021-08-26 17:26:05 +0300

    Add max value to amount field

--- App/View/Templates/basket.php
+++ App/View/Templates/basket.php
@@ -2,6 +2,7 @@
     <div class="container">
         <div class="basket">
             <p class="p_basket">Корзина <i class="fas fa-cart-plus"></i></p>
+            <?php if (isset($_SESSION['universalError'])) echo $_SESSION['universalError'] ?>
             <?php if (empty($vars['books'])) : ?>
                 <div>
                     <h2 class="text-center">Корзина пока пуста</h2>
@@ -37,7 +38,7 @@
                                 </button>
                             </form>
                             <form class="d-inline" action="basket/buy" method="POST">
-                                <input name="amount" type="number" step="1" min="1" placeholder="<?php echo $list['amount']?>">
+                                <input name="amount" type="number" step="1" min="1" max="<?php echo $book->getAmount() ?>" placeholder="<?php echo $list['amount']?>">
                                 <button name="slug" class="buy" type="submit" value="<?php echo $book->getSlug() ?>">
                                     Купить
                                 </button>
